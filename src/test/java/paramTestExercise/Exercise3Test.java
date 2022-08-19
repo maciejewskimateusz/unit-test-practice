592ea44ab1aade3eb280b120b4692752e0723a61
Mateusz Maciejewski <m.maciejewski93@gmail.com>
2022-08-19 21:29:05 +0200

    init

---
+++ src/test/java/paramTestExercise/Exercise3Test.java
@@ -0,0 +1,58 @@
+package paramTestExercise;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.YearMonth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Exercise3Test {
+
+    private Exercise3 exercise = new Exercise3();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2001-01", "2001-03", "2001-05", "2001-07", "2001-08", "2001-10", "2001-12"})
+    public void shouldReturn31Days(String month) {
+        // given
+        YearMonth yearMonth = YearMonth.parse(month);
+
+        // when
+        int dayCount = exercise.getDayCount(yearMonth.getYear(), yearMonth.getMonthValue());
+
+        // then
+        assertThat(dayCount).isEqualTo(31);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2001, 04", "2001, 06", "2001, 11", "2001, 09"})
+    public void shouldReturn30Days(int year, int month) {
+
+        // when
+        int dayCount = exercise.getDayCount(year, month);
+
+        // then
+        assertThat(dayCount).isEqualTo(30);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2021, 02"})
+    public void shouldReturn28Days(int year, int month) {
+        // when
+        int dayCount = exercise.getDayCount(year, month);
+
+        // then
+        assertThat(dayCount).isEqualTo(28);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2004, 02", "2000, 02"})
+    public void shouldReturn29Days(int year, int month) {
+        // when
+        int dayCount = exercise.getDayCount(year, month);
+
+        // then
+        assertThat(dayCount).isEqualTo(29);
+    }
+}
